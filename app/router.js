e49c5f9d9fc024e46ecfbd947e929d715669a798
Nagasundaram <nagasundram@gmail.com>
2018-04-25 11:16:38 +0530

    Dev (#1)
    * Added Scientist route
    
    * Tried Super heros
    
    * Basics
    Routing
    Root URl(index url)
    Rendering partial
    Links to

--- app/router.js
+++ app/router.js
@@ -7,6 +7,11 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('scientists');
+  this.route('super-heros');
+  this.route('about');
+  this.route('contact');
+  this.route('books');
 });
 
 export default Router;
